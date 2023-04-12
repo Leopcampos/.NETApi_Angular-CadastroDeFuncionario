c1366ebad5abfb3965ba4d3e43860e7024f5786d
Leo Campos <leopcampos85@gmail.com>
2023-04-12 18:41:59 -0300

    Aul 16 finalizada

--- Frontend/projetoFuncionarios/src/app/consulta-dependentes/consulta-dependentes.component.ts
+++ Frontend/projetoFuncionarios/src/app/consulta-dependentes/consulta-dependentes.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-consulta-dependentes',
   templateUrl: './consulta-dependentes.component.html',
   styleUrls: ['./consulta-dependentes.component.css']
 })
-export class ConsultaDependentesComponent {
+export class ConsultaDependentesComponent implements OnInit {
 
+  //atributos
+  listagemDependentes: any[] = [];
+  mensagem: string = "";
+
+  //declarando e inicializando o httpClient
+  constructor(private httpClient: HttpClient) { }
+
+  //função executada quando o componente é carregado
+  ngOnInit(): void {
+    this.consultarDependentes(); //executando a consulta
+  }
+
+  //função para realizar a consulta de dependentes
+  consultarDependentes(): void {
+    this.httpClient.get<any[]>(environment.apiUrl + "/dependentes")
+      .subscribe(
+        (data: any[]) => {
+          this.listagemDependentes = data;
+        },
+        e => {
+          console.log(e);
+        }
+      );
+  }
+
+  //função para excluir o dependente
+  excluirDependente(id: any): void {
+
+    if (window.confirm('Deseja realmente excluir o dependente?')) {
+
+      //realizando a chamada de exclusão da API..
+      this.httpClient.delete(environment.apiUrl + '/dependentes/' + id,
+        { responseType: 'text' })
+        .subscribe(
+          data => {
+            this.mensagem = data;
+            this.consultarDependentes();
+          },
+          e => {
+            console.log(e);
+          }
+        );
+    }
+  }
+
+  //função para limpar as mensagens..
+  limparMensagens(): void {
+    this.mensagem = "";
+  }
 }
